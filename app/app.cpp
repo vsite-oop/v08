4cf4ba8d29a5151604678b43080eb9a8ec80412c
Filip Lončar <filoncar@vsite.hr>
2022-12-19 20:39:53 +0100

    app.h update

--- app/app.cpp
+++ app/app.cpp
@@ -2,5 +2,27 @@
 
 namespace vsite::oop::v8
 {
+	int vsite::oop::v8::input_num(std::istream& is) throw (BaseExcp) {
 
+	}
+	char vsite::oop::v8::input_op(std::istream& is) throw (BaseExcp) {
+
+	}
+	double vsite::oop::v8::calc(const int first, const char op, const int second) throw (BaseExcp) {
+
+	}
+
+
+	class vsite::oop::v8::BaseExcp {
+	public:
+		virtual std::string error() {};
+	};
+	class vsite::oop::v8::not_number : public vsite::oop::v8::BaseExcp {
+	public:
+		std::string error() override {
+		}
+	};
+	class vsite::oop::v8::not_operator : public vsite::oop::v8::BaseExcp {};
+	class vsite::oop::v8::divide_zero : public vsite::oop::v8::BaseExcp {};
+}
 }
--- app/app.h
+++ app/app.h
@@ -4,13 +4,13 @@
 
 namespace vsite::oop::v8 
 {
-	int input_num(std::stringstream& ss);
-	char input_op(std::stringstream& ss);
+	int input_num(std::istream& is);
+	char input_op(std::istream& is);
 	double calc(const int first, const char op, const int second);
 
 
-	class BaseExcp{};
-	class not_number : public BaseExcp{};
-	class not_operator : public BaseExcp{};
-	class divide_zero : public BaseExcp{};
+	class BaseExcp { virtual std::string error(); };
+	class not_number : public BaseExcp { std::string error() override; };
+	class not_operator : public BaseExcp { std::string error() override; };
+	class divide_zero : public BaseExcp { std::string error() override; };
 }
