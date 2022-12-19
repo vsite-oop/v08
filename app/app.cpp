b52ca76762f3c0163e2537a7df869781519c34ef
Deni Kiseljak <dekiseljak@vsite.hr>
2022-12-19 20:57:40 +0100

    initial commit

--- app/app.cpp
+++ app/app.cpp
@@ -2,5 +2,11 @@
 
 namespace vsite::oop::v8
 {
+	
+	int input_num(std::stringstream& number) {
+	};
+	std::string input_op(std::stringstream& operation) {
+		
+	};
 
 }
--- app/app.h
+++ app/app.h
@@ -1,6 +1,9 @@
 #pragma once
+#include <string>
 
 namespace vsite::oop::v8 
 {
+	int input_num(std::stringstream& number);
+	std::string input_op(std::stringstream& operation);
 
 }
