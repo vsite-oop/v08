ddb468fb287aac07b5037c3fc52f8ba940a99f94
MislavBucar <mislavbucar@gmail.com>
2022-12-20 17:30:46 +0100

    ne znam kak da implementiram exception

--- app/app.cpp
+++ app/app.cpp
@@ -1,6 +1,46 @@
 #include "app.h"
+#include <iostream>
 
 namespace vsite::oop::v8
 {
-
+	int input_num(std::istream& ss) {
+		int num;
+		ss >> num;
+		return num;
+	}
+	char input_op(std::istream& ss)
+	{
+		char chr;
+		ss >> chr;
+		return chr;
+	}
+	double calc(int num1, char ch, int num2)
+	{
+		switch (ch)
+		{
+		case '+':
+			return num1 + num2;
+			break;
+		case '-':
+			return num1 - num2;
+			break;
+		case '/':
+			return num1 / num2;
+			break;
+		case '*':
+			return num1 * num2;
+			break;
+		default:
+			break;
+		}
+	}
+	not_number::not_number() : mText("You haven't used a number")
+	{
+	}
+	not_operator::not_operator() : mText("You haven't used an operator")
+	{
+	}
+	divide_zero::divide_zero() : mText("You tried using 0 in diving")
+	{
+	}
 }
--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,18 @@
 #include "app.h"
+#include<iostream>
+using namespace vsite::oop::v8;
+
 
 // read and evaluate expression inside endless loop 
 // break after first exception (use one catch clause), show error description
 
 int main()
 {
+	while (true) {
+		int num, num2;
+		char ch;
+
+		std::cin >> num >> ch >> num2;
+		std::cout << calc(num, ch, num2) << std::endl;
+	}
 }
