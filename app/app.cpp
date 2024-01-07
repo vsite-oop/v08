d9cf48bffc54b51e1bd37e08b88aa0f4c050d470
Josip Grabušić <jograbus@vsite.hr>
2024-01-07 16:48:54 +0100

    v08 solved

--- app/app.cpp
+++ app/app.cpp
@@ -1,6 +1,38 @@
 #include "app.h"
+#include <string>  // Dodajte ovu liniju kako biste omoguæili upotrebu std::string
+using namespace std;  // Dodajte ovu liniju kako biste mogli koristiti sufix s
 
 namespace vsite::oop::v8
 {
+    int input_num(istream& is) {
+        int n;
+        is >> n;
+        if (is.fail()) {
+            throw not_number();
+        }
+        return n;
+    }
 
+    char input_op(istream& is)
+    {
+        char c;
+        is >> c;
+        if ("*/+-"s.find(c) == string::npos)
+            throw not_operator();
+        return c;
+    }
+
+    double calc(int broj1, char opera, int broj2)
+    {
+        switch (opera) {
+        case '+': return broj1 + broj2;
+        case '-': return broj1 - broj2;
+        case '*': return broj1 * broj2;
+        case '/':
+            if (broj2 == 0)
+                throw divide_zero();
+            return broj1 / static_cast<double>(broj2);
+        default:  throw not_operator();
+        }
+    }
 }
--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,24 @@
 #include "app.h"
+using namespace vsite::oop::v8;
 
-// read and evaluate expression inside endless loop 
-// break after first exception (use one catch clause), show error description
 
 int main()
 {
+	while (true)
+	{
+		try
+		{
+			auto broj1 = input_num(std::cin);
+			auto opera = input_op(std::cin);
+			auto broj2 = input_num(std::cin);
+
+			double rez = calc(broj1, opera, broj2);
+			std::cout << broj1 << opera << broj2 << '=' << rez << std::endl;
+		}
+		catch (const base_exception& e)
+		{
+			std::cout << e.what();
+			break;
+		}
+	}
 }
