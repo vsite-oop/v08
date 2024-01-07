73c12ae8e9dec3d54a119a47573497f75fd4d082
Hrvoje Janđel <hrjandje@vsite.hr>
2024-01-07 21:09:04 +0100

    vj8 resolved

--- app/app.cpp
+++ app/app.cpp
@@ -1,6 +1,43 @@
 #include "app.h"
-
+using namespace std;
+#include <string> 
 namespace vsite::oop::v8
 {
+    int input_num(istream& is) {
+        int n;
+        is >> n;
+        if (is.fail()) {
+            throw not_number();
+        }
+        return n;
+    }
+    char input_op(istream& is)
+    {
+        char c;
+        is >> c;
+        if ("*/+-"s.find(c) == string::npos)
+            throw not_operator();
+        return c;
+    }
 
-}
+    double calc(int num1, char op, double num2)
+    {
+        switch (op) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+
+            if (num2 == 0)
+                throw divide_zero();
+
+
+            else {
+                return num1 / num2;
+            }
+        }
+    }
+    }
--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,25 @@
 #include "app.h"
-
+using namespace vsite::oop::v8;
 // read and evaluate expression inside endless loop 
 // break after first exception (use one catch clause), show error description
 
 int main()
 {
+	while (true)
+	{
+		try
+		{
+			auto num1 = input_num(std::cin);
+			auto op = input_op(std::cin);
+			auto num2 = input_num(std::cin);
+
+			double rez = calc(num1, op, num2);
+			std::cout << num1 << op << num2 << '=' << rez << std::endl;
+		}
+		catch (const base_exception& e)
+		{
+			std::cout << e.what();
+			break;
+		}
+	}
 }
