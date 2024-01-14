4c0a7ff30eb809e8f8790c2489646f8f143d5edf
Tin Bašić <tin.basic22@gmail.com>
2024-01-14 15:23:45 +0100

    Lab8

--- app/app.cpp
+++ app/app.cpp
@@ -2,5 +2,46 @@
 
 namespace vsite::oop::v8
 {
+    int input_num(std::istream& is) {
+        int n;
+        is >> n;
+        if (is.fail()) {
+            throw not_number();
+        }
+        return n;
+    }
+
+    char input_op(std::istream& is)
+    {
+        int op = -1;
+        char c;
+        is >> c;
+
+        const std::string operators = "/*-+";
+
+        op = operators.find(c);
+        if (op == -1)
+            throw not_operator();
+
+        return c;
+    }
+
+    double calc(int fNum, char op, int lNum)
+    {
+        switch (op) {
+        case '+':
+            return fNum + lNum;
+        case '-':
+            return fNum - lNum;
+        case '*':
+            return fNum * lNum;
+        case '/':
+            if (lNum == 0)
+                throw divide_zero();
+            return fNum / static_cast<double>(lNum);
+        default:
+            throw not_operator();
+        }
+    }
 
 }
--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,28 @@
 #include "app.h"
 
+#include "app.h"
+using namespace vsite::oop::v8;
+
 // read and evaluate expression inside endless loop 
 // break after first exception (use one catch clause), show error description
 
 int main()
 {
+
+	while (true)
+	{
+		try
+		{
+			auto f = input_num(std::cin);
+			auto op = input_op(std::cin);
+			auto l = input_num(std::cin);
+
+			double r = calc(f, op, l);
+			std::cout << f << op << l << '=' << r << std::endl;
+		}
+		catch (const base_exception& e)
+		{
+			std::cout << e.what();
+			break;
+		}
 }
