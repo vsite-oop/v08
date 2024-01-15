dc725eb3aca8e4e8f862aa1a2b9515350790fcd2
Sonja Perković <soperkov@vsite.hr>
2024-01-15 21:01:40 +0100

    sperkovic Vjezba 8

--- app/app.cpp
+++ app/app.cpp
@@ -1,6 +1,43 @@
 #include "app.h"
+#include <string>
+#include <iostream>
 
 namespace vsite::oop::v8
 {
+	int input_num(std::istream& s) {
+		int n;
+		s >> n;
+		if (s.fail()) {
+			throw not_number();
+		}
+		return n;
+	}
 
+	char input_op(std::istream& s) {
+		char op;
+		std::string sss = "+-*/";
+		s >> op;
+		if (sss.find(op) == std::string::npos)
+			throw not_operator();
+		return op;
+		
+	}
+
+	double calc(int num1, char op, int num2) {
+		switch (op) {
+		case '+':
+			return num1 + num2;
+		case '-':
+			return num1 - num2;
+		case '*':
+			return num1 * num2;
+		case '/':
+			if (num2 == 0) {
+				throw divide_zero();
+			}
+			return static_cast<double>(num1) / num2;
+		default:
+			throw not_operator();
+		}
+	}
 }
--- app/app.h
+++ app/app.h
@@ -1,6 +1,40 @@
 #pragma once
+#include <string>
+#include <iostream>
+
 
 namespace vsite::oop::v8 
 {
+	int input_num(std::istream& s);
 
+	char input_op(std::istream& s);
+
+	double calc(int num1, char op, int num2);
+
+	class base_exception{
+	public:
+		virtual std::string message() const = 0;
+		virtual ~base_exception() = default;
+	};
+
+	class not_operator : public base_exception {
+	public:
+		std::string message() const override {
+			return "not an operator!";
+		}
+	};
+
+	class not_number : public base_exception {
+	public:
+		std::string message() const override {
+			return "not a number!";
+		}
+	};
+
+	class divide_zero : public base_exception {
+	public:
+		std::string message() const override {
+			return "cannot divide by zero!";
+		}
+	};
 }
